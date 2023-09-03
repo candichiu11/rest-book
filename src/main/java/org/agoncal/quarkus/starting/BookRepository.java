9fb5f4b322c1c6a7fcca8ec103cbed8973dddf7f
Candi Lilun Chiu <candi.chiu@handyapp.co.uk>
2023-09-03 17:42:05 +0200

    Implement BookResourceTest

--- src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -1,12 +1,6 @@
 package org.agoncal.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +9,7 @@ public class BookRepository {
 
     public List<Book> getAllBooks() {
         return List.of(
-            new Book(1, "Thinking in Jave", "Bruce Eckel", 1998, "IT"),
+            new Book(1, "Thinking in Java", "Bruce Eckel", 1998, "IT"),
             new Book(2, "Effective Java", "Josh Bloch", 2001, "IT"),
             new Book(3, "Practicing Quarkus", "Antonio",2020, "IT"),
             new Book(4, "Understanding Quarkus", "Antonio", 2020, "IT")
--- src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -1,7 +1,10 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
+
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -10,12 +13,39 @@ import static org.hamcrest.CoreMatchers.is;
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldGetAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+        when()
+             .get("/api/books").
+        then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body("size()", is(4));
     }
 
+    @Test
+    public void shouldCountAllBooks() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN).
+                when()
+                .get("/api/books/count").
+                then()
+                .statusCode(200)
+                .body(is("4"));
+    }
+
+    @Test
+    public void shouldGetABooks() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1).
+        when()
+                .get("/api/books/{id}").
+        then()
+                .statusCode(200)
+                .body("title", is("Thinking in Java"))
+                .body("author", is("Bruce Eckel"))
+                .body("yearOfPublication", is(1998))
+                .body("genre", is("IT"));
+    }
 }
