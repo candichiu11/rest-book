ba1847dcc7c475ca80099ef549598b89f7b35f49
Candi Lilun Chiu <candi.chiu@handyapp.co.uk>
2023-09-04 18:24:27 +0200

    configure the application

--- src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -1,18 +1,22 @@
 package org.agoncal.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "book.genre", defaultValue = "Sci-Fi")
+    String genre;
     public List<Book> getAllBooks() {
         return List.of(
-            new Book(1, "Thinking in Java", "Bruce Eckel", 1998, "IT"),
-            new Book(2, "Effective Java", "Josh Bloch", 2001, "IT"),
-            new Book(3, "Practicing Quarkus", "Antonio",2020, "IT"),
-            new Book(4, "Understanding Quarkus", "Antonio", 2020, "IT")
+            new Book(1, "Thinking in Java", "Bruce Eckel", 1998, genre),
+            new Book(2, "Effective Java", "Josh Bloch", 2001, genre),
+            new Book(3, "Practicing Quarkus", "Antonio",2020, genre),
+            new Book(4, "Understanding Quarkus", "Antonio", 2020, genre)
         );
     }
 
