bbd98c5153db65442a98bc1fca4abb44a83ad85a
Candi Lilun Chiu <candi.chiu@handyapp.co.uk>
2023-09-07 17:40:50 +0200

    Add some configuration and tests

--- src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
@@ -1,8 +1,27 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
 public class BookResourceIT extends BookResourceTest {
-    // Execute the same tests but in packaged mode.
+    @Test
+    public void shouldGetABooks() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1).
+                when()
+                .get("/api/books/{id}").
+                then()
+                .statusCode(200)
+                .body("title", is("Thinking in Java"))
+                .body("author", is("Bruce Eckel"))
+                .body("yearOfPublication", is(1998))
+                .body("genre", is("non-fiction"));
+    }
 }
